35e3665623214ec48e439b82aa1c281055703076
andrewbak37 <91086922+andrewbak37@users.noreply.github.com>
2022-10-31 23:34:02 +0300

    add-docker

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
@@ -10,11 +10,13 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareit.booking.dto.BookItemRequestDto;
 import ru.practicum.shareit.booking.dto.BookingState;
 import ru.practicum.shareit.client.BaseClient;
+import ru.practicum.shareit.exception.ValidationException;
 
 import java.util.Map;
 
 @Service
 public class BookingClient extends BaseClient {
+
     private static final String API_PREFIX = "/bookings";
 
     @Autowired
@@ -27,7 +29,9 @@ public class BookingClient extends BaseClient {
         );
     }
 
-    public ResponseEntity<Object> getBookings(long userId, BookingState state, Integer from, Integer size) {
+    public ResponseEntity<Object> getBookings(long userId, String stateParam, Integer from, Integer size) {
+        BookingState state = BookingState.from(stateParam)
+                .orElseThrow(() -> new ValidationException("Unknown state: " + stateParam));
         Map<String, Object> parameters = Map.of(
                 "state", state.name(),
                 "from", from,
@@ -36,8 +40,29 @@ public class BookingClient extends BaseClient {
         return get("?state={state}&from={from}&size={size}", userId, parameters);
     }
 
+    public ResponseEntity<Object> getOwnerBookings(long userId, String stateParam, Integer from, Integer size) {
+        BookingState state = BookingState.from(stateParam)
+                .orElseThrow(() -> new ValidationException("Unknown state: " + stateParam));
+        Map<String, Object> parameters = Map.of(
+                "state", state,
+                "from", from,
+                "size", size
+        );
+        return get("/owner?state={state}&from={from}&size={size}", userId, parameters);
+    }
+
+    public ResponseEntity<Object> approveBooking(long userId, long bookingId, Boolean approved) {
+        Map<String, Object> parameters = Map.of(
+                "approved", approved
+        );
+        // /bookings/5?approved=true
+        return patch("/" + bookingId + "?approved={approved}", userId, parameters, null);
+    }
 
     public ResponseEntity<Object> bookItem(long userId, BookItemRequestDto requestDto) {
+        if (requestDto.getStart().isAfter(requestDto.getEnd())) {
+            throw new ValidationException("Validation Failed");
+        }
         return post("", userId, requestDto);
     }
 
